e00269959cadd4da7c60019846e9b992aa9eee19
cruedy <cruedy@hmc.edu>
2024-10-29 11:56:24 -0700

    Add port listening

--- myenv/FoodIdentifier.py
+++ myenv/FoodIdentifier.py
@@ -76,3 +76,5 @@ def findFood():
         print("Error occurred in findFood:", str(e))
         return jsonify({"error": str(e)}), 500
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
